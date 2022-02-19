d4dec7e443c69fdf26db0e3387994c2b70e33c19
jonathan <jonathanfernandesjf98@gmail.com>
2022-02-19 00:52:03 -0300

    feat: validaçao da search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,33 @@
-
+import react, { useState, useEffect } from 'react';
 
 function SearchBar() {
+  const [input, setInput] = useState('');
+  const [isDisable, setIsDisable] = useState(true);
+
+  useEffect(() => {
+    validateEmail(input)
+    console.log(input)
+    console.log(isDisable)
+  }, [input])
+
+  const validateEmail = (text) => {
+    const regex = /[~`!@#$%^&*()+={}\[\];:\'\"<>.,\/\\\?-_]/g;
+    if (text.length < 3) {
+      setIsDisable(true)
+    }
+    else {
+      text.match(regex) ? setIsDisable(true) : setIsDisable(false)
+    }
+  }
+
+  const handleChange = ({ target }) => {
+    setInput(target.value)
+  }
+
   return (
     <div className="search-bar-container">
-      <input type="text" placeholder="Qual o mimo de hoje para o seu anjinho?"/>
-      <button type="button" className="search-button">Buscar</button>
+      <input type="text" value={input} onChange={handleChange} placeholder="Qual o mimo de hoje para o seu anjinho?"/>
+      <button type="button" className="search-button" disabled={isDisable} >Buscar</button>
     </div>
   )
 }
